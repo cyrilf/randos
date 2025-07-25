19d297fca1b0477d0afe23bd8c8977220a9ffc4a
cyrilf <1302282+cyrilf@users.noreply.github.com>
2025-07-25 15:05:00 +0200

    feat: update admin refresh page to handle github rebuild

--- app/pages/admin/refresh.vue
+++ app/pages/admin/refresh.vue
@@ -3,60 +3,78 @@ import type { FormSubmitEvent } from "@nuxt/ui";
 import type { Toast } from "@nuxt/ui/runtime/composables/useToast.js";
 
 const state = reactive({
-  hookId: "",
+  accessToken: "",
 });
+
 const isSuccess = ref(false);
 
-async function sendHookRequest(hookId: string) {
-  const { error } = await useFetch(`https://api.netlify.com/build_hooks/${hookId}`, { method: "POST" });
-
-  let toastContent: Partial<Toast> = {
-    title: "Erreur",
-    description: "Le hook id est invalide ou le site est déjà en cours de déploiement.",
-    color: "error",
-  };
-
-  if (!error.value) {
+async function sendHookRequest(accessToken: string) {
+  try {
+    await $fetch(`https://api.github.com/repos/cyrilf/randos/actions/workflows/deploy.yml/dispatches`, {
+      method: "POST",
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${accessToken}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+      body: {
+        ref: "main",
+      },
+    });
     isSuccess.value = true;
-    toastContent = {
-      title: "Succès",
-      description: "Le site est en cours de redéploiement.",
-      color: "success",
+  } catch {
+    isSuccess.value = false;
+  } finally {
+    let toastContent: Partial<Toast> = {
+      title: "Erreur",
+      description: "L'access token est invalide ou le site est déjà en cours de déploiement.",
+      color: "error",
     };
-    // Remove the hookId from the state to prevent resubmission
+
+    if (isSuccess.value) {
+      toastContent = {
+        title: "Succès",
+        description: "Le site est en cours de redéploiement.",
+        color: "success",
+      };
+    }
+
+    const toast = useToast();
+    toast.add(toastContent);
+
+    // Remove the accessToken from the state to prevent resubmission
     // and from the query parameters
-    state.hookId = "";
+    state.accessToken = "";
     const route = useRoute();
     await navigateTo({ path: route.path, replace: true });
   }
-
-  const toast = useToast();
-  toast.add(toastContent);
 }
 
 onMounted(async () => {
-  const { hookId } = useRoute().query;
-  if (hookId) {
-    await sendHookRequest(hookId as string);
+  const { accessToken } = useRoute().query;
+  if (accessToken) {
+    await sendHookRequest(accessToken as string);
   }
 });
 
-async function onSubmit(event: FormSubmitEvent<{ hookId: string }>) {
-  const hookId = event.data.hookId;
-  await sendHookRequest(hookId);
+async function onSubmit(event: FormSubmitEvent<{ accessToken: string }>) {
+  const accessToken = event.data.accessToken;
+  await sendHookRequest(accessToken);
 }
 </script>
 
 <template>
-  <div class="my-16 flex flex-col items-center justify-between">
-    <h1 class="text-2xl font-bold">Re-déployer le site</h1>
-    <UForm v-if="!isSuccess" :state="state" class="mt-8 space-y-4" @submit="onSubmit">
-      <UFormField label="Hook id" name="hookId">
-        <UInput v-model="state.hookId" type="password" />
-      </UFormField>
+  <div class="my-16">
+    <UCard variant="subtle" color="primary" class="mx-auto w-fit p-8">
+      <h1 class="text-2xl font-bold">Re-déployer le site</h1>
+      <UForm v-if="!isSuccess" :state class="mt-8 space-y-4" @submit="onSubmit">
+        <UFormField label="Access token" name="accessToken">
+          <UInput v-model="state.accessToken" type="password" />
+        </UFormField>
 
-      <UButton type="submit"> Re-déployer </UButton>
-    </UForm>
-    <div v-else class="mt-8 text-green-600">Le site est en cours de redéploiement.</div>
+        <UButton type="submit"> Re-déployer </UButton>
+      </UForm>
+      <div v-else class="mt-8 text-green-600">Le site est en cours de redéploiement.</div>
+    </UCard>
   </div>
 </template>
